5a9a3e361b43ec8c0e3be18829b4b22ea34ca84d
Amayyas <amayyas.aouadene@epitech.eu>
2026-04-27 18:31:45 +0200

    feat: implement thread primitives setup function (Resolves #3)

---
+++ src/init.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2026
+** Panoramix
+** File description:
+** setup resources
+*/
+
+#include "panoramix.h"
+
+int init_resources(common_t *common, params_t *params)
+{
+    common->params = *params;
+    common->current_pot = params->pot_size;
+    common->refills_left = params->nb_refills;
+    if (pthread_mutex_init(&common->pot_mutex, NULL) != 0)
+        return (84);
+    if (pthread_mutex_init(&common->print_mutex, NULL) != 0)
+        return (84);
+    if (sem_init(&common->druid_sleep, 0, 0) != 0)
+        return (84);
+    if (sem_init(&common->villager_wait, 0, 0) != 0)
+        return (84);
+    return (0);
+}
