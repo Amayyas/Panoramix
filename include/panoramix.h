0a34b2677c9352d105d28e46d376e659d926a96a
Amayyas <amayyas.aouadene@epitech.eu>
2026-04-27 17:34:33 +0200

    feat: declare params struct and parsing prototype (Resolves #2)

--- include/panoramix.h
+++ include/panoramix.h
@@ -8,4 +8,16 @@
 #ifndef PANORAMIX_H_
     #define PANORAMIX_H_
 
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    typedef struct params_s {
+        int nb_villagers;
+        int pot_size;
+        int nb_fights;
+        int nb_refills;
+    } params_t;
+
+    int parse_args(int ac, char **av, params_t *params);
+
 #endif /* !PANORAMIX_H_ */
