1406c548420614e79fa27cc61e2858e7547c1760
Amayyas <amayyas.aouadene@epitech.eu>
2026-04-27 17:39:28 +0200

    fix: correct header indentation for Epitech coding style C-L2

--- include/panoramix.h
+++ include/panoramix.h
@@ -11,13 +11,13 @@
     #include <stdio.h>
     #include <stdlib.h>
 
-    typedef struct params_s {
-        int nb_villagers;
-        int pot_size;
-        int nb_fights;
-        int nb_refills;
-    } params_t;
+typedef struct params_s {
+    int nb_villagers;
+    int pot_size;
+    int nb_fights;
+    int nb_refills;
+} params_t;
 
-    int parse_args(int ac, char **av, params_t *params);
+int parse_args(int ac, char **av, params_t *params);
 
 #endif /* !PANORAMIX_H_ */
