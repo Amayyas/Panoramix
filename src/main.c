c00a838b88787ff0d4515a963394ab04cb0c5170
Amayyas <amayyas.aouadene@epitech.eu>
2026-04-27 17:34:33 +0200

    feat: implement argument parsing and error handling (Resolves #2)

--- src/main.c
+++ src/main.c
@@ -7,9 +7,36 @@
 
 #include "panoramix.h"
 
+static void print_usage(void)
+{
+    printf("USAGE: ./panoramix <nb_villagers> <pot_size> ");
+    printf("<nb_fights> <nb_refills>\n");
+}
+
+int parse_args(int ac, char **av, params_t *params)
+{
+    if (ac != 5) {
+        print_usage();
+        return (84);
+    }
+    params->nb_villagers = atoi(av[1]);
+    params->pot_size = atoi(av[2]);
+    params->nb_fights = atoi(av[3]);
+    params->nb_refills = atoi(av[4]);
+    if (params->nb_villagers <= 0 || params->pot_size <= 0 ||
+        params->nb_fights <= 0 || params->nb_refills <= 0) {
+        print_usage();
+        printf("Values must be >0.\n");
+        return (84);
+    }
+    return (0);
+}
+
 int main(int ac, char **av)
 {
-    (void)ac;
-    (void)av;
+    params_t params = {0};
+
+    if (parse_args(ac, av, &params) == 84)
+        return (84);
     return (0);
 }
