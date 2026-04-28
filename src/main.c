dda04b7579e4f838950e1e15535d3e8caf0998bb
Amayyas <amayyas.aouadene@epitech.eu>
2026-04-28 17:31:39 +0200

    feat: launch and join threads in main (Resolves #5)

--- src/main.c
+++ src/main.c
@@ -32,11 +32,33 @@ int parse_args(int ac, char **av, params_t *params)
     return (0);
 }
 
+static void launch_villagers(villager_t *villagers, common_t *common)
+{
+    for (int i = 0; i < common->params.nb_villagers; i++) {
+        villagers[i].id = i;
+        villagers[i].fights_left = common->params.nb_fights;
+        villagers[i].common = common;
+        pthread_create(&villagers[i].thread, NULL, villager_thread,
+            &villagers[i]);
+    }
+}
+
 int main(int ac, char **av)
 {
     params_t params = {0};
+    common_t common = {0};
+    druid_t druid = {0};
+    villager_t *villagers = NULL;
 
-    if (parse_args(ac, av, &params) == 84)
+    if (parse_args(ac, av, &params) == 84 ||
+        init_resources(&common, &params) == 84)
         return (84);
+    druid.common = &common;
+    pthread_create(&druid.thread, NULL, druid_thread, &druid);
+    villagers = malloc(sizeof(villager_t) * params.nb_villagers);
+    launch_villagers(villagers, &common);
+    for (int i = 0; i < params.nb_villagers; i++)
+        pthread_join(villagers[i].thread, NULL);
+    free(villagers);
     return (0);
 }
