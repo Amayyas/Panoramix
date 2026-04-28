c6dbb055584305271d656be6d72fafb3cb6a9c19
Amayyas <amayyas.aouadene@epitech.eu>
2026-04-28 17:31:39 +0200

    feat: implement villager thread logic (Resolves #5)

---
+++ src/villager.c
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2026
+** Panoramix
+** File description:
+** villager logic
+*/
+
+#include "panoramix.h"
+
+void *villager_thread(void *arg)
+{
+    villager_t *v = (villager_t *)arg;
+
+    printf("Villager %d: Going into battle!\n", v->id);
+    while (v->fights_left > 0) {
+        pthread_mutex_lock(&v->common->pot_mutex);
+        printf("Villager %d: I need a drink... I see %d servings left.\n",
+            v->id, v->common->current_pot);
+        if (v->common->current_pot == 0) {
+            printf("Villager %d: Hey Pano wake up! We need more potion.\n",
+                v->id);
+            sem_post(&v->common->druid_sleep);
+            sem_wait(&v->common->villager_wait);
+        }
+        v->common->current_pot--;
+        pthread_mutex_unlock(&v->common->pot_mutex);
+        v->fights_left--;
+        printf("Villager %d: Take that roman scum! Only %d left.\n",
+            v->id, v->fights_left);
+    }
+    printf("Villager %d: I'm going to sleep now.\n", v->id);
+    return (NULL);
+}
